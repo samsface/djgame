28c6b5a160b0d06ce4bfcad2fafc485a5bc49e80
Sam <samsface@gmail.com>
2024-06-25 00:13:03 +0200

    stuff

--- demo/game/world_viewport.gd
+++ demo/game/world_viewport.gd
@@ -10,21 +10,18 @@ func _ready() -> void:
 func _invalidate_graphics_quality() -> void:
 	match Bus.config_service.graphics_quality:
 		ConfigService.GraphicsQuality.HIGH:
-			stretch_shrink = 1
 			subviewport_.msaa_3d = Window.MSAA_8X
 			subviewport_.screen_space_aa = Window.SCREEN_SPACE_AA_FXAA
 			subviewport_.use_taa = false
 			subviewport_.scaling_3d_scale = 1.0
 			get_tree().call_group("light", "set_visible", true)
 		ConfigService.GraphicsQuality.LOW:
-			stretch_shrink = 1
 			subviewport_.msaa_3d = Window.MSAA_DISABLED
 			subviewport_.screen_space_aa = Window.SCREEN_SPACE_AA_DISABLED
 			subviewport_.use_taa = false
 			subviewport_.scaling_3d_scale = 0.5
 			get_tree().call_group("light", "set_visible", false)
 		ConfigService.GraphicsQuality.POTATO:
-			stretch_shrink = 1
 			subviewport_.msaa_3d = Window.MSAA_DISABLED
 			subviewport_.screen_space_aa = Window.SCREEN_SPACE_AA_DISABLED
 			subviewport_.use_taa = false
