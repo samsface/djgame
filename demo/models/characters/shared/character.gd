36ce9def05e4ec59b8c290adfe5ebc7e9bd90455
Sam <samsface@gmail.com>
2024-07-31 12:36:15 +0200

    sam

--- demo/models/characters/shared/character.gd
+++ demo/models/characters/shared/character.gd
@@ -4,11 +4,11 @@ class_name Character
 
 var r := randf()
 
-@export_range(0.0, 10.0) var speed_scale:float  = 1.0 :
+@export_range(0.0, 3.0) var time_scale:float  = 1.0 :
 	set(v):
-		speed_scale = v
+		time_scale = v
 		if is_node_ready():
-			$AnimationTree.set("parameters/Speed/scale", v)
+			$AnimationTree.set("parameters/TimeScale/scale", v)
 	
 @export_range(0.0, 1.0) var filming:float :
 	set(v):
@@ -20,49 +20,42 @@ var r := randf()
 		if anger == 0 and v != 0:
 			$AnimationTree.set("parameters/AngerTimeSeek/seek_request", randf() * 19.0)
 		anger = v
-		invalidate_attention_()
+
 
 @export_range(0.0, 1.0) var attention:float :
 	set(v):
 		attention = v
-		invalidate_attention_()
+		if is_node_ready():
+			$AnimationTree.set("parameters/Jump/blend_position", attention)
 
-@export_range(0.0, 1.0) var cheer:float :
+@export_range(0.0, 1.0) var cheer_left:float :
 	set(v):
-		cheer = v
-		invalidate_attention_()
-		
+		cheer_left = v
+		if is_node_ready():
+			$AnimationTree.set("parameters/CheerLeft/blend_amount", cheer_left)
+
+@export_range(0.0, 1.0) var cheer_right:float :
+	set(v):
+		cheer_right = v
+		if is_node_ready():
+			$AnimationTree.set("parameters/CheerRight/blend_amount", cheer_right)
+
 @export_range(0.0, 1.0) var clap:float :
 	set(v):
 		clap = v
-		invalidate_attention_()
+
 
 @export_range(0.0, 1.0) var tired:float :
 	set(v):
 		tired = v
-		invalidate_attention_()
 
-func invalidate_attention_() -> void:
-	if not is_node_ready():
-		return
 
-	var a := []
-	var n := 3
-	for i in 3:
-		a.push_back(clamp((attention - (i/float(n))) * float(n), 0.0, 1.0))
-
-	$AnimationTree.set("parameters/AttentionA/blend_amount", a[0])
-	$AnimationTree.set("parameters/AttentionB/blend_amount", a[1])
-	$AnimationTree.set("parameters/AttentionC/blend_amount", a[2])
-	
-	$AnimationTree.set("parameters/Cheer/blend_amount", cheer)
-	$AnimationTree.set("parameters/Clap/blend_amount", clap)
-	$AnimationTree.set("parameters/Tired/blend_amount", tired)
-	$AnimationTree.set("parameters/Anger/blend_amount", anger)
 
 var delta_sum_ := 0.0
 
 func _physics_process(delta: float) -> void:
+	
+	AnimationNodeBlendTree
 	delta_sum_ += delta
 	
 	##if sin(delta_sum_ * 40.0) > 0.9:
@@ -70,6 +63,6 @@ func _physics_process(delta: float) -> void:
 	#else:
 	#	speed_scale = 0.0
 
-	if not Engine.is_editor_hint():
+	if not Engine.is_editor_hint() or true:
 		if has_node("Armature"):
 			$Armature.position = $AnimationTree.get_root_motion_position_accumulator()
